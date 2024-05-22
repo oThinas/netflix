57c66aa483cfeca904a3bc98f428ad993a0b3f24
oThinas <thiagomartinsprado@gmail.com>
2024-05-22 17:45:53 -0300

    feat: add toast on favorite action

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -7,6 +7,7 @@ import { AiOutlineCheck, AiOutlinePlus } from 'react-icons/ai';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
 import { useFavorites } from '@/hooks/useFavorites';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 interface FavoriteButtonProps {
   movieId: string;
@@ -41,13 +42,21 @@ export function FavoriteButton(props: FavoriteButtonProps) {
     mutateFavorites();
   }, [props.movieId, isFavorite, currentUser, mutate, mutateFavorites]);
 
+  const handleFavorite = useCallback(() => {
+    toast.promise(toggleFavorites(), {
+      error: 'Failed to update favorite',
+      loading: 'Updating favorite...',
+      success: 'Favorite updated!',
+    });
+  }, [toggleFavorites]);
+
   const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
 
   return (
     <div
       className='group/item flex size-6 cursor-pointer items-center justify-center rounded-full border-2 border-white
       transition hover:border-neutral-300 lg:size-10'
-      onClick={() => toggleFavorites()}
+      onClick={() => handleFavorite()}
     >
       <Icon className='text-white group-hover/item:text-neutral-300' size={24} />
     </div>
